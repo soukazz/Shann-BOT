12a2a16282c6dd3beb0b774db489b3dfd8397ba1
Rafael Rigo <>
2021-04-02 20:41:02 +0000

    Added many more commmands

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import random
 from keep_alive import keep_alive
 
 client = discord.Client()
-
-lipe = ['My Little ponny :3', 'EEEW que monstro!']
+lipelist = [
+    'My Little ponny :3', 'EEEW que monstro!', 'quem é esse?', 'Meu amiguinho',
+    ':face_vomiting:', 'Melhor senpai de todos!', 'Esse é O CARA :sunglasses:',
+    'humilde esse cara',
+    'Fiz um poema pra ele!\n**O Lipin\nÉ meu amiguin\nÉ um pouco doidin\nMas é legalzin!**',
+    'Trato como se fosse gente!', 'Moleque irado, come isopor :thumbsup:'
+]
 
 
 @client.event
@@ -17,93 +22,121 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    lipe = [
-        'My Little ponny :3', 'EEEW que monstro!', 'quem é esse?',
-        'Meu amiguinho', ':face_vomiting:', 'Melhor senpai de todos!',
-        'Esse é O CARA :sunglasses:', 'humilde esse cara',
-        'Fiz um poema pra ele!\n**O Lipin\nÉ meu amiguin\nÉ um pouco doidin\nMas é legalzin!**',
-        'Trato como se fosse gente!', 'Moleque irado, come isopor :thumbsup:'
-    ]
-    lipe = random.choice(lipe)
+    lipe = random.choice(lipelist)
+    msg = message.content
+
     if message.author == client.user:
         return
-    if message.content.startswith('.hi'):  # hi command
+    if msg.startswith('s.hi'):  # hi command
         await message.channel.send('Hello!')
 
-    elif message.content.startswith('.hello'):  # hello command
+    elif msg.startswith('s.hello'):  # hello command
         await message.channel.send('Hi!')
 
-    elif message.content.startswith('.good morning'):  # good morning command
+    elif msg.startswith('s.good morning'):  # good morning command
         await message.channel.send('Good morning sunflower!')
 
-    elif message.content.startswith('.info'):  # information command
+    elif msg.startswith('s.info'):  # information command
         pass
 
-    elif message.content.startswith('.help'):  # help command
+    elif msg.startswith('s.help'):  # help command
         await message.channel.send(
-            '**Hey! If you use this command is because you need help! So those are my commands!**\n\n`1 - hi = Hello!\n2 - hello =  Hi!\n3 - good morning =  Good morning sunflower!\n4 - help = u know what it does, dont?\n5 - .die = YOU die :rage:\n6 - .ping = Pong!\n7 - .favorite food = Idk, i dont eat, im just a robot :3\n8 - .good night = Good night lunarflower\n9 - .date = NOOOOO, im ugly right now, and imma BOT ;[` \n \n**Anyway, my prefix is "."**'
+            '**Hey! If you use this command is because you need help! So those are my commands!**\n\n1 - hi = Hello!\n2 - hello =  Hi!\n3 - good morning =  Good morning sunflower!\n4 - help = u know what it does, dont?\n5 - die = YOU die :rage:\n6 - ping = Pong!\n7 - favorite food = Idk, i dont eat, im just a robot :3\n8 - good night = Good night lunarflower\n9 - date = NOOOOO, im ugly right now, and imma BOT ;[` \n \n**Anyway, my prefix is "s."**'
         )
 
-    elif message.content.startswith(
-            '.spam'):  # spam command that spams 25 messages
+    elif msg.startswith('s.spam'):  # spam command that spams 25 messages
         for x in range(25):
             await message.channel.send(
                 "MEGA SPAM OF 666 666 666 666 666 666 666 666 666 666 666 ")
 
-    elif message.content.startswith(
-            ".sp4n"):  # spam command that spams 100 messages
+    elif msg.startswith("s.sp4n"):  # spam command that spams 100 messages
         for x in range(100):
             await message.channel.send(
                 ".span100 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666 666"
             )
-    elif message.content.startswith('.kazz'):  # good morning command
+    elif msg.startswith('s.kazz'):  # good morning command
         await message.channel.send(
             'The most beutifull, pretty, awesome, perfect, cleany, good person, rich, hot, etc.'
         )
 
     # Fun commands
-    elif message.content.startswith('.die'):
+    elif msg.startswith('s.die'):
         await message.channel.send('YOU die :rage:')
 
-    elif message.content.startswith('.shut up'):
+    elif msg.startswith('s.shut_up'):
         await message.channel.send('NO WAY :rage: :rage:')
 
-    elif message.content.startswith('.ping'):
+    elif msg.startswith('s.ping'):
         await message.channel.send('Pong!')
 
-    elif message.content.startswith('.favorite food'):
+    elif msg.startswith('s.favorite food'):
         await message.channel.send('Idk, i dont eat, im just a bot :3')
 
-    elif message.content.startswith('.good night'):
+    elif msg.startswith('s.good night'):
         await message.channel.send('Good night lunarflower')
 
-    elif message.content.startswith('.shann is cute'):
+    elif msg.startswith('s.shann is cute'):
         await message.channel.send('positive! :3')
 
-    elif message.content.startswith('.date'):
+    elif msg.startswith('s.date'):
         await message.channel.send('NOOOOO, im ugly right now, and imma BOT ;['
                                    )
 
-    elif message.content.startswith('.shann'):
+    elif msg.startswith('s.shann'):
         await message.channel.send('Tell me cutie :3 !')
 
-    elif message.content.startswith(
-            '.lipe'):  # special command for our friend lipe :)
+    elif msg.startswith('s.lipe'):  # special command for our friend lipe :)
         await message.channel.send(lipe)
 
-    elif message.content == '.infinity':
+    elif msg == 's.infinity':
         await message.channel.send('$infinity')
 
-    elif message.content == '.kill':
+    elif msg == 's.kill':
         await message.channel.send(
             'Mission accept! Im going to kill my target sir!')
 
-    elif message.content == '.b':
+    elif msg == 's.b':
         await message.channel.send(
             'É aquele lá, aquele que nasceu aleijado? É, esse mesmo!')
 
-    elif message.content == '.enrico':
+    elif msg == 's.enrico':
         await message.channel.send("Um ótimo amigo!")
+
+    elif msg.startswith('s.ppt'):
+        choices = ['pedra', 'papel', 'tesoura']
+        bot = random.choice(choices).lower()
+        player = str(msg[6:])
+
+        if player == bot:
+            await message.channel.send("Você escolheu " + player + " e eu escolhi " + bot + ".")
+            await message.channel.send('Deu um empate! :clap:')
+        
+        elif player == 'pedra':
+            if bot == 'tesoura':
+                await message.channel.send("Você escolheu " + player + " e eu escolhi " + bot + ".")
+                await message.channel.send('Você venceu! :sob:')
+            else:
+                await message.channel.send("Você escolheu " + player + " e eu escolhi " + bot + ".")
+                await message.channel.send("Eu venci! :stuck_out_tongue_winking_eye:")
+        
+        elif player == "tesoura":
+            if bot == "papel":
+                await message.channel.send("Você escolheu " + player + " e eu escolhi " + bot + ".")
+                await message.channel.send("Você venceu! :sob:")
+            else:
+                await message.channel.send("Você escolheu " + player + " e eu escolhi " + bot + ".")
+                await message.channel.send("Eu venci! :stuck_out_tongue_winking_eye:")
+
+        elif player == "papel":
+            if bot == "pedra":
+                await message.channel.send("Você escolheu " + player + " e eu escolhi " + bot + ".")
+                await message.channel.send("Você venceu! :sob:")
+            else:
+                await message.channel.send("Você escolheu " + player + " e eu escolhi " + bot + ".")
+                await message.channel.send("Eu venci! :stuck_out_tongue_winking_eye:")
+
+        else:
+            await message.channel.send("Essa jogada não existe bobino(a)! :laughing:")
 
 
 keep_alive()
